74552bf9490741531e7064e69ed68894e5064d45
Fareedh7 <fareedhahamed07@gmail.com>
2025-09-14 19:02:30 +0530

    Initial commit

---
+++ Day_2/factorial.js
@@ -0,0 +1,7 @@
+// To calculate factorial of a given number
+
+var fact = 1;
+for (i = 1; i <= 5; i++) {
+  fact *= i;
+}
+console.log(fact);
---
+++ Day_2/sumEvenNum.js
@@ -0,0 +1,9 @@
+// To print the sum of Even numbers greater than 10 less than 30
+
+var sum =0
+
+for(i=10;i<=30;i+=2){
+    sum +=i;
+}
+console.log(sum);
+// console.log(i);
